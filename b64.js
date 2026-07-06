5d9bbcd162afc4068dfb18874947048ea00d379b
Philioraptor <dhruvm0506@gmail.com>
2026-07-06 22:16:34 +0530

    Remove b64 scratch script

--- b64.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const env = fs.readFileSync('.env.local', 'utf8');
-const match = env.match(/FIREBASE_SERVICE_ACCOUNT_KEY='(.*?)'/s);
-if (match) {
-  console.log(Buffer.from(match[1]).toString('base64'));
-}
